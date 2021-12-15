17b3a4ed93afb4c3cd5c594f3c952c52efb0a94c
peter senaj <peter.senaj@pelikan.sk>
2021-12-15 10:50:59 +0100

    first commit of spring backend app

---
+++ src/main/java/com/pitrsen/meetyoursportmate/MeetyoursportmateApplication.java
@@ -0,0 +1,13 @@
+package com.pitrsen.meetyoursportmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetyoursportmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetyoursportmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitrsen/meetyoursportmate/MeetyoursportmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitrsen.meetyoursportmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetyoursportmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
