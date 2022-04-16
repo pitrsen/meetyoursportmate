cde273790744208b151eba46c97ef9aa91490035
peter senaj <peter.senaj@pelikan.sk>
2022-04-16 22:33:04 +0200

    [SA-12] - entity City

--- src/main/java/com/pitrsen/meetyoursportmate/entity/City.java
+++ src/main/java/com/pitrsen/meetyoursportmate/entity/City.java
@@ -11,14 +11,14 @@ import javax.persistence.Id;
 /**
  * Created by IntelliJ IDEA.
  * User: peterSenaj
- * Date: 15.4.2022
- * Time: 21:35
+ * Date: 16.4.2022
+ * Time: 22:30
  * Project: meetyoursportmate
  */
 @Getter
 @Setter
 @Entity
-public class Sport {
+public class City {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
