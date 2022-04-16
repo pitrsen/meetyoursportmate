48a8e928ebb70ecd6803530993037e566dbf05c5
peter senaj <peter.senaj@pelikan.sk>
2022-04-16 08:23:45 +0200

    [SA-6] Sport entity, config postgresql

---
+++ src/main/java/com/pitrsen/meetyoursportmate/entity/Sport.java
@@ -0,0 +1,29 @@
+package com.pitrsen.meetyoursportmate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: peterSenaj
+ * Date: 15.4.2022
+ * Time: 21:35
+ * Project: meetyoursportmate
+ */
+@Getter
+@Setter
+@Entity
+public class Sport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+}
