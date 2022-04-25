73ed4ac80f82ea136e017b70d2d771c5075a071f
peter senaj <peter.senaj@pelikan.sk>
2022-04-25 19:54:25 +0200

    [SA-26] - create ActivityId class

---
+++ src/main/java/com/pitrsen/meetyoursportmate/entity/ActivityId.java
@@ -0,0 +1,25 @@
+package com.pitrsen.meetyoursportmate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: peterSenaj
+ * Date: 25.4.2022
+ * Time: 19:53
+ * Project: meetyoursportmate
+ */
+@Embeddable
+public class ActivityId {
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "sport_id")
+    private Long sportId;
+
+    @Column(name = "city_id")
+    private Long cityId;
+
+}
